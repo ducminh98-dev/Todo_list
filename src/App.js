a5d19976d1e587f38cc7b12163a9c81ab6db26aa
Duc Minh <minh98it@gmail>
2020-03-24 23:12:30 +0700

    create react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TodoItem from '../src/component/todoItem'
+
+class App extends Component {
+
+  constructor(){
+    super()
+
+    this.todoItem = [
+      "Rua Tay",
+      "Rua mat",
+      "Rua chan"
+    ]
+
+
+  }
+
+  render(){
+
+    return (
+      <div className="App">
+        {this.todoItem.map((item, index) => <TodoItem key={index} title={item}/>)}
+
+      </div>
+    );
+
+  }
 }
 
 export default App;
